61489cfb0c8418f43e4090d30e982168bb3ed36c
preetsuthar17 <preetsutharxd@gmail.com>
2023-09-17 13:56:28 +0530

    chore: fix reading time calculator in post page

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -56,11 +56,15 @@ function calculateReadTime(content) {
   const seconds = Math.round((readTimeMinutes % 1) * 60);
 
   let readTime = "";
+
   if (hours > 0) {
     readTime += `${hours} hour${hours > 1 ? "s" : ""} `;
   }
-  if (remainingMinutes > 0) {
-    readTime += `${remainingMinutes} minute${remainingMinutes > 1 ? "s" : ""} `;
+
+  if (remainingMinutes > 0 || (hours === 0 && seconds > 0)) {
+    readTime += `${Math.max(1, remainingMinutes)} minute${
+      remainingMinutes > 1 ? "s" : ""
+    } `;
   }
 
   return readTime.trim();
