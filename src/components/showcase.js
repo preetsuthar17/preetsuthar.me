3251c40bf81677e63f603dca7068bcf0c7936732
Preet Suthar <preetsutharxd@gmail.com>
2023-09-20 05:25:14 +0000

    fix: typo

--- src/components/showcase.js
+++ src/components/showcase.js
@@ -249,11 +249,11 @@ const Showcase = () => {
                 <span className="text-only-outline">front-end devloper</span>
               </div>
               <div className="text">
-                <span className="text-only-outline">front-end devloper</span>
-                <span className="text-only-outline">front-end devloper</span>
-                <span className="text-only-outline">front-end devloper</span>
-                <span className="text-only-outline">front-end devloper</span>
-                <span className="text-only-outline">front-end devloper</span>
+                <span className="text-only-outline">front-end developer</span>
+                <span className="text-only-outline">front-end developer</span>
+                <span className="text-only-outline">front-end developer</span>
+                <span className="text-only-outline">front-end developer</span>
+                <span className="text-only-outline">front-end developer</span>
               </div>
             </div>
             <div className="scrolling_text" style={{
