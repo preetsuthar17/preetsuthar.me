5dc04d88298028e42619124608a3bf09c458eea5
Preet Suthar <preetsutharxd@gmail.com>
2023-12-27 09:28:33 +0530

    style: adjust button style

--- pages/tags/[tag].js
+++ pages/tags/[tag].js
@@ -147,24 +147,41 @@ const Tag = ({ blogs }) => {
                       </h2>
                     </Link>
                     <p className="blog-text">
-                      {getFirstFewLines(blog.content)}{" "}
+                      {getFirstFewLines(blog.content)}...{" "}
                     </p>
-                    <p
-                      className="blog-text blog-date"
-                      style={{
-                        fontSize: "0.9rem",
-                      }}
-                    >
-                      {blog.date}
-                    </p>
-                    <div
-                      style={{
-                        marginTop: "0.6rem",
-                      }}
-                    >
-                      <Link href={`/posts/${blog.slug}`} passHref>
-                        Read article &rarr;
+                    <div>
+                      <Link
+                        href={`/posts/${blog.slug}`}
+                        className="blog-read-link"
+                        passHref
+                      >
+                        Read{" "}
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="16"
+                          height="16"
+                          fill="currentColor"
+                          class="bi bi-box-arrow-in-up-right"
+                          viewBox="0 0 16 16"
+                        >
+                          <path
+                            fill-rule="evenodd"
+                            d="M6.364 13.5a.5.5 0 0 0 .5.5H13.5a1.5 1.5 0 0 0 1.5-1.5v-10A1.5 1.5 0 0 0 13.5 1h-10A1.5 1.5 0 0 0 2 2.5v6.636a.5.5 0 1 0 1 0V2.5a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 .5.5v10a.5.5 0 0 1-.5.5H6.864a.5.5 0 0 0-.5.5"
+                          />
+                          <path
+                            fill-rule="evenodd"
+                            d="M11 5.5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793l-8.147 8.146a.5.5 0 0 0 .708.708L10 6.707V10.5a.5.5 0 0 0 1 0z"
+                          />
+                        </svg>
                       </Link>
+                      <p
+                        className="blog-text blog-date"
+                        style={{
+                          fontSize: "0.9rem",
+                        }}
+                      >
+                        {blog.date}
+                      </p>
                     </div>
                   </div>
                 </motion.div>
