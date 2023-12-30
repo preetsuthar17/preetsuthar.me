47cd3fc6cd83e628763cc1c078a209db07faccbd
Preet Suthar <preetsutharxd@gmail.com>
2023-12-30 17:11:24 +0530

    chore: add buymeacoffee button

--- pages/_app.js
+++ pages/_app.js
@@ -79,6 +79,18 @@ export default function App({ Component, pageProps }) {
           gtag('config', 'G-PXPK7RW6DG');
         `}
           </Script>
+          <script
+            data-name="BMC-Widget"
+            data-cfasync="false"
+            src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
+            data-id="preetsuthar"
+            data-description="Support me on Buy me a coffee!"
+            data-message=""
+            data-color="#FF813F"
+            data-position="Right"
+            data-x_margin="18"
+            data-y_margin="18"
+          ></script>
         </>
         {/* <CourseSnackBar /> */}
         <CustomCursor />
