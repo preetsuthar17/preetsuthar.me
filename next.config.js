8bcd6f7dadfa842af3d02f61f430aae7724c9780
Preet Suthar <preetsutharxd@gmail.com>
2024-01-13 10:07:49 +0530

    feat: switch to bun js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const fs = require("fs-extra");
 const path = require("path");
 const matter = require("gray-matter");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   sassOptions: {
@@ -14,23 +15,9 @@ module.exports = {
   ],
   webpack: (config, { isServer }) => {
     if (isServer) {
-      const articlesDir = path.join(__dirname, "articles");
-      const files = fs.readdirSync(articlesDir);
-
-      files.forEach((filename) => {
-        const filePath = path.join(articlesDir, filename);
-        const fileContents = fs.readFileSync(filePath, "utf8");
-        const { data } = matter(fileContents);
-        const slug = generateSlug(data.title);
-
-        const newFilePath = path.join(articlesDir, `${slug}.md`);
-        if (filename !== `${slug}.md`) {
-          fs.renameSync(filePath, newFilePath);
-        }
-        console.log(`slug renamed to ${slug}.md`);
-      });
-
       generateSitemap();
+      config.optimization.minimizer.push(new TerserPlugin());
+      console.log("Optimized project!");
     }
 
     return config;
@@ -45,64 +32,22 @@ function generateSlug(title) {
     .trim();
 }
 
-function generateSitemap() {
+function renameSlug() {
   const articlesDir = path.join(__dirname, "articles");
   const files = fs.readdirSync(articlesDir);
-  const dynamicPaths = files.map((filename) => {
-    const { data } = matter(
-      fs.readFileSync(path.join(articlesDir, filename), "utf8")
-    );
+
+  files.forEach((filename) => {
+    const filePath = path.join(articlesDir, filename);
+    const fileContents = fs.readFileSync(filePath, "utf8");
+    const { data } = matter(fileContents);
     const slug = generateSlug(data.title);
-    return `/posts/${slug}`;
+
+    const newFilePath = path.join(articlesDir, `${slug}.md`);
+    if (filename !== `${slug}.md`) {
+      fs.renameSync(filePath, newFilePath);
+    }
+    console.log(`slug renamed to ${slug}.md`);
   });
-
-  const tagPages = getTagPages();
-  const tagPaths = tagPages.map((tag) => `/tags/${tag}`);
-
-  const sitemap = `
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    
-  <url>
-    <loc>https://preetsuthar.me</loc>
-    <changefreq>weekly</changefreq>
-    <priority>1.0</priority>
-  </url>
-  <url>
-    <loc>https://preetsuthar.me/about</loc>
-    <changefreq>weekly</changefreq>
-  </url>
-  <url>
-    <loc>https://preetsuthar.me/projects</loc>
-    <changefreq>monthly</changefreq>
-  </url>
-  <url>
-    <loc>https://preetsuthar.me/posts</loc>
-    <priority>1.0</priority>
-    <changefreq>daily</changefreq>
-  </url>
-    ${dynamicPaths
-      .map(
-        (path) =>
-          `<url><loc>https://preetsuthar.me${path}</loc><priority>1.0</priority><changefreq>daily</changefreq></url>`
-      )
-      .join("")}
-      ${tagPaths
-        .map(
-          (tagPath) =>
-            `<url><loc>https://preetsuthar.me${tagPath}</loc><priority>1.0</priority><changefreq>weekly</changefreq></url>`
-        )
-        .join("")}
-
-    </urlset>
-  `;
-
-  fs.writeFileSync(
-    path.join(__dirname, "public", "sitemap.xml"),
-    sitemap,
-    "utf8"
-  );
-
-  console.log("Sitemap generated successfully!");
 }
 
 function getTagPages() {
@@ -121,4 +66,64 @@ function getTagPages() {
   });
 
   return Array.from(tags);
+}
+
+function generateSitemap() {
+  const articlesDir = path.join(__dirname, "articles");
+  const files = fs.readdirSync(articlesDir);
+  const dynamicPaths = files.map((filename) => {
+    const { data } = matter(
+      fs.readFileSync(path.join(articlesDir, filename), "utf8")
+    );
+    const slug = generateSlug(data.title);
+    return `/posts/${slug}`;
+  });
+
+  const tagPages = getTagPages();
+  const tagPaths = tagPages.map((tag) => `/tags/${tag}`);
+
+  const sitemap = `
+      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      
+    <url>
+      <loc>https://preetsuthar.me</loc>
+      <changefreq>weekly</changefreq>
+      <priority>1.0</priority>
+    </url>
+    <url>
+      <loc>https://preetsuthar.me/about</loc>
+      <changefreq>weekly</changefreq>
+    </url>
+    <url>
+      <loc>https://preetsuthar.me/projects</loc>
+      <changefreq>monthly</changefreq>
+    </url>
+    <url>
+      <loc>https://preetsuthar.me/posts</loc>
+      <priority>1.0</priority>
+      <changefreq>daily</changefreq>
+    </url>
+      ${dynamicPaths
+        .map(
+          (path) =>
+            `<url><loc>https://preetsuthar.me${path}</loc><priority>1.0</priority><changefreq>daily</changefreq></url>`
+        )
+        .join("")}
+        ${tagPaths
+          .map(
+            (tagPath) =>
+              `<url><loc>https://preetsuthar.me${tagPath}</loc><priority>1.0</priority><changefreq>weekly</changefreq></url>`
+          )
+          .join("")}
+  
+      </urlset>
+    `;
+
+  fs.writeFileSync(
+    path.join(__dirname, "public", "sitemap.xml"),
+    sitemap,
+    "utf8"
+  );
+
+  console.log("Sitemap generated successfully!");
 }
