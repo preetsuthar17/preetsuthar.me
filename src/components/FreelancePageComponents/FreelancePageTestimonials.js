e19bfc90b156536b6e944df63b409b3a697dd91c
Preet Suthar <preetsutharxd@gmail.com>
2024-03-04 10:13:54 +0000

    Add header and Testimonials component in Freelance page

---
+++ src/components/FreelancePageComponents/FreelancePageTestimonials.js
@@ -0,0 +1,14 @@
+export const FreelancePageTestimonials = () => {
+    return (
+            <section class="freelancing-page-testimonials">
+                <h2>Testimonials</h2>
+                <div class="freelancing-page-testimonials-cards-list">
+                    <div class="freelancing-page-testimonials-card">
+                       
+                    </div>
+
+
+                </div>
+            </section>
+    )
+}
