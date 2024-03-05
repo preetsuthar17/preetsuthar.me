3f102401ff8f5d1e3d57e4bf66fe6e8cff5db755
Preet Suthar <preetsutharxd@gmail.com>
2024-03-05 15:29:32 +0530

    Add scroll animation to About component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,37 @@
+import { About } from "@/components/About";
 import { Hero } from "@/components/Hero";
+import { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Home = () => {
+  const aboutRef = useRef(null);
+  const heroRef = useRef(null);
+
+  useEffect(() => {
+    gsap.fromTo(
+      aboutRef.current,
+
+      { y: "0%" },
+      {
+        y: "-180%",
+        boxShadow: "0px -68px 129px -88px rgba(0,0,0,5.75)",
+        scrollTrigger: {
+          trigger: aboutRef.current,
+          start: "top bottom",
+          end: "bottom top",
+          scrub: true,
+        },
+      }
+    );
+  }, []);
+
   return (
     <>
       <Hero />
+      <About ref={aboutRef} />
     </>
   );
 };
