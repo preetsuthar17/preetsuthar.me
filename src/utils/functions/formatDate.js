997caeeff92c9edd6e3b0496fc6c4f6880024fe5
Preet Suthar <preetsutharxd@gmail.com>
2024-03-05 07:13:29 +0530

    Remove existing files and create new project

--- src/utils/functions/formatDate.js
+++
@@ -1,9 +0,0 @@
-const formatDate = (date) => {
-  const options = { month: "short", day: "2-digit", year: "numeric" };
-  const formattedDate = new Intl.DateTimeFormat("en-US", options).format(
-    new Date(date)
-  );
-  return formattedDate;
-};
-
-export default formatDate;
