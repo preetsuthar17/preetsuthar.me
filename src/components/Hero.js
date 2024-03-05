7173dd2ee7cbf613510393fad1c01a4bba14c5a4
Preet Suthar <preetsutharxd@gmail.com>
2024-03-05 14:34:14 +0530

    Update homepage with new hero component, adjust font sizes, and add text outline effect

---
+++ src/components/Hero.js
@@ -0,0 +1,19 @@
+export const Hero = () => {
+  return (
+    <section className="hero">
+      <div className="section-1">
+        <p className="hero-main-heading">
+          <span className="text-only-outline">HEY THERE</span>
+          <span className="orange-color">,</span>
+        </p>
+        <h1 className="hero-main-heading"> I'M PREET</h1>
+      </div>
+      <div className="section-2">
+        <p>
+          I'm a freelance front-end developer and designer, crafting experiences
+          that exceed expectations.
+        </p>
+      </div>
+    </section>
+  );
+};
