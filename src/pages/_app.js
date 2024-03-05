c591cbcb526d2caddbdf437e8b0e133b15c9e17a
Preet Suthar <preetsutharxd@gmail.com>
2024-03-05 07:29:26 +0530

    Add Layout component and update dependencies

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,19 @@
-import "@/styles/globals.css";
+import { Layout } from "@/components/Layout";
+import "@/styles/globals.scss";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link
+          href="https://api.fontshare.com/v2/css?f[]=satoshi@900,700,500,300,400&f[]=switzer@800,900&f[]=clash-grotesk@200,700,400,600,300,500&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
 }
