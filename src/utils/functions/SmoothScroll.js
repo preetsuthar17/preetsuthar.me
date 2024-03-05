781def2f79ab43a8991508bf413e3f55e7eacd46
Preet Suthar <preetsutharxd@gmail.com>
2024-03-05 17:01:07 +0530

    Update duration for SmoothScroll component

--- src/utils/functions/SmoothScroll.js
+++ src/utils/functions/SmoothScroll.js
@@ -2,7 +2,7 @@ import ReactLenis from "@studio-freight/react-lenis";
 
 export const SmoothScroll = ({ children }) => {
   return (
-    <ReactLenis root options={{ duration: 1.2 }}>
+    <ReactLenis root options={{ duration: 1.5 }}>
       {children}
     </ReactLenis>
   );
