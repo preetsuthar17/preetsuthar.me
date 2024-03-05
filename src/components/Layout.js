c2e1e0e0cdb89449c9768217212a03b8447879ab
Preet Suthar <preetsutharxd@gmail.com>
2024-03-05 15:29:27 +0530

    Add scroll animation to footer

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,30 @@
 import { Footer } from "./Footer";
 import { Navbar } from "./Navbar";
+import { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 export const Layout = ({ children }) => {
+  const footerRef = useRef(null);
+
+  useEffect(() => {
+    gsap.fromTo(
+      footerRef.current,
+      { y: "0" },
+      {
+        y: "0",
+        scrollTrigger: {
+          trigger: footerRef.current,
+          start: "top bottom",
+          end: "bottom top",
+          scrub: true,
+        },
+      }
+    );
+  }, []);
+
   return (
     <>
       <Navbar />
