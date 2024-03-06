8d0fcb4249645692b3dbeff2569d84bcde21a81c
Preet Suthar <preetsutharxd@gmail.com>
2024-03-06 16:56:56 +0530

    Refactor Layout component to remove unused code

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,30 +1,7 @@
 import { Footer } from "./Footer";
 import { Navbar } from "./Navbar";
-import { useEffect, useRef } from "react";
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
-
-gsap.registerPlugin(ScrollTrigger);
 
 export const Layout = ({ children }) => {
-  const footerRef = useRef(null);
-
-  useEffect(() => {
-    gsap.fromTo(
-      footerRef.current,
-      { y: "0" },
-      {
-        y: "0",
-        scrollTrigger: {
-          trigger: footerRef.current,
-          start: "top bottom",
-          end: "bottom top",
-          scrub: true,
-        },
-      }
-    );
-  }, []);
-
   return (
     <>
       <span id="home"></span>
