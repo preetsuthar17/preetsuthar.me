d7acbd74e35242031b68602f43cfd9ca25b65446
Preet Suthar <preetsutharxd@gmail.com>
2024-03-06 14:27:57 +0530

    Add floating blocks to Hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,8 +1,21 @@
 import React from "react";
+import { Parallax } from "react-scroll-parallax";
 
 export const Hero = React.forwardRef((props, ref) => {
   return (
     <section className="hero" ref={ref}>
+      <div className="floating-blocks-1">
+        <Parallax
+          translateY={[-100, 50]}
+          rotateZ={[0, 360]}
+          className="square-1"
+        ></Parallax>
+        <Parallax
+          translateY={[50, -100]}
+          rotateZ={[0, 360]}
+          className="circle-1"
+        ></Parallax>
+      </div>
       <div className="section-1">
         <p className="hero-main-heading">
           <span className="text-only-outline">HEY THERE</span>
