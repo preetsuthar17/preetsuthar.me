a74b4aefdf81bc44ce4aae5932efe581f1679fda
Preet Suthar <preetsutharxd@gmail.com>
2024-03-07 21:30:13 +0530

    Add rate limiting to sendMail API

--- src/pages/api/sendMail.js
+++ src/pages/api/sendMail.js
@@ -1,6 +1,39 @@
 import { Resend } from "resend";
 import { EmailTemplate } from "@/components/other/EmailTemplate";
 
+const requestCounts = {};
+
+export const rateLimit = (req, res, next) => {
+  const ip = req.socket.remoteAddress;
+  const currentTime = Date.now();
+
+  if (!requestCounts[ip]) {
+    requestCounts[ip] = {
+      count: 1,
+      lastRequestTime: currentTime,
+    };
+  } else {
+    const elapsedTime = currentTime - requestCounts[ip].lastRequestTime;
+
+    if (elapsedTime < 60000) {
+      requestCounts[ip].count++;
+    } else {
+      requestCounts[ip] = {
+        count: 1,
+        lastRequestTime: currentTime,
+      };
+    }
+  }
+
+  if (requestCounts[ip].count > 5) {
+    return res
+      .status(429)
+      .json({ error: "Rate limit exceeded. Try again later." });
+  }
+
+  next();
+};
+
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export default async function handler(req, res) {
