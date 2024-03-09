e2a50440a908bde0493e15a88eca57ff8f64f75f
Preet Suthar <preetsutharxd@gmail.com>
2024-03-09 13:25:56 +0530

    Update font styles and families

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,12 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <link
-          rel="stylesheet"
-          href="https://api.fontshare.com/v2/css?f[]=satoshi@900,700,500,300,400&f[]=switzer@800,900&f[]=clash-grotesk@200,700,400,600,300,500&display=swap"
-        />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
