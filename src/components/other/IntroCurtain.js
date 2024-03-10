bf299427834d14184c24a818a6552f472c858bd2
Preet Suthar <preetsutharxd@gmail.com>
2024-03-10 13:47:03 +0530

    Refactor IntroCurtain component and update intro curtain animation delay

--- src/components/other/IntroCurtain.js
+++ src/components/other/IntroCurtain.js
@@ -1,12 +1,52 @@
-import gsap from "gsap";
+import { useState, useEffect } from "react";
 
 export const IntroCurtain = () => {
-  const date = new Date();
-  const year = date.getFullYear();
+  const greetings = [
+    "Hello",
+    "નમસ્તે",
+    "CIAO",
+    "こんにちは",
+    "Hola",
+    "안녕",
+    "Bonjour",
+    "Olá",
+    "Hallo",
+    "你好",
+    "سلام",
+    "Merhaba",
+    "Γεια σας",
+    "Shalom",
+    "Salam",
+    "Hej",
+    "Hei",
+    "Cześć",
+    "Ahoj",
+    "สวัสดี",
+    "Xin chào",
+    "Selam",
+    "Jambo",
+    "नमस्ते",
+  ];
+  const [currentGreeting, setCurrentGreeting] = useState(greetings[0]);
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalDuration = 200 - index * 10;
+    const interval = setInterval(() => {
+      setIndex((prevIndex) => {
+        const newIndex =
+          prevIndex + 1 === greetings.length ? prevIndex : prevIndex + 1;
+        setCurrentGreeting(greetings[newIndex]);
+        return newIndex;
+      });
+    }, intervalDuration);
+
+    return () => clearInterval(interval);
+  }, [index]);
+
   return (
     <div className="intro-curtain">
-      <h1>Preet Suthar</h1>
-      <p>&copy; {year}</p>
+      <h1>{currentGreeting}</h1>
     </div>
   );
 };
