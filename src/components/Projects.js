8c0f443c50ab4546dece357aeb149bd66ef1f641
Preet Suthar <preetsutharxd@gmail.com>
2024-03-10 21:23:14 +0530

    Refactor project card layout in Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -55,48 +55,12 @@ const ProjectCard = ({ image, title, content, numbering, projectLink }) => {
   };
 
   return (
-    <>
-      <div
-        onMouseEnter={() => setIsHovered(true)}
-        onMouseLeave={() => setIsHovered(false)}
-        onClick={handleClick}
-        className="project-card"
-      >
-        <div className="project-card-image">
-          <Image
-            src={image}
-            width={1920}
-            height={1080}
-            alt={title}
-            loading="lazy"
-            quality={100}
-            ref={imageRef}
-          />
-        </div>
-        <div className="project-card-content">
-          <p className="project-card-numbering">{numbering} -</p>
-          <h3 className="project-card-heading">{title}</h3>
-          <p className="project-card-content">{content}</p>
-          <Link
-            href={projectLink}
-            target="_blank"
-            className="secondary-button project-card-link"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-            >
-              <path
-                fill="currentColor"
-                d="M5 21q-.825 0-1.412-.587T3 19V5q0-.825.588-1.412T5 3h7v2H5v14h14v-7h2v7q0 .825-.587 1.413T19 21zm4.7-5.3l-1.4-1.4L17.6 5H14V3h7v7h-2V6.4z"
-              ></path>
-            </svg>{" "}
-            View project
-          </Link>
-        </div>
-      </div>
+    <div
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      onClick={handleClick}
+      className="project-card"
+    >
       <div
         ref={cursorRef}
         className="project-card-custom-cursor"
@@ -128,7 +92,41 @@ const ProjectCard = ({ image, title, content, numbering, projectLink }) => {
           />
         </svg>
       </div>
-    </>
+      <div className="project-card-image">
+        <Image
+          src={image}
+          width={1920}
+          height={1080}
+          alt={title}
+          loading="lazy"
+          quality={100}
+          ref={imageRef}
+        />
+      </div>
+      <div className="project-card-content">
+        <p className="project-card-numbering">{numbering} -</p>
+        <h3 className="project-card-heading">{title}</h3>
+        <p className="project-card-content">{content}</p>
+        <Link
+          href={projectLink}
+          target="_blank"
+          className="secondary-button project-card-link"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+          >
+            <path
+              fill="currentColor"
+              d="M5 21q-.825 0-1.412-.587T3 19V5q0-.825.588-1.412T5 3h7v2H5v14h14v-7h2v7q0 .825-.587 1.413T19 21zm4.7-5.3l-1.4-1.4L17.6 5H14V3h7v7h-2V6.4z"
+            ></path>
+          </svg>{" "}
+          View project
+        </Link>
+      </div>
+    </div>
   );
 };
 
@@ -168,10 +166,24 @@ export const Projects = React.forwardRef((props, ref) => {
       <div className="projects-content">
         <div className="projects-cards-lists" style={projectCardsListStyle}>
           <ProjectCard
+            image="https://i.imgur.com/TWXkv4D.png"
+            title="Vultrex Website"
+            content="A freelance project to develop the Vultrex website, delivered within a tight 3-day deadline. The website showcases Vultrex's services and capabilities, enhancing its online presence."
+            numbering="1"
+            projectLink="https://beta.vultrex.dev/"
+          />
+          <ProjectCard
+            image="https://i.imgur.com/F8uNCoz.png"
+            title="Ayaan Designs"
+            content="A collaborative project with Product Designer, Ayaan. I developed a website to showcase his portfolio, using modern web technologies to ensure visual appeal and robust functionality."
+            numbering="2"
+            projectLink="https://ayaandesigns.xyz/"
+          />
+          <ProjectCard
             image="https://i.imgur.com/FO5kakY.png"
             title="Snippix"
             content="Snippix allows you to convert your boring code blocks in beautiful images and let your export as PNG with so many customization options. "
-            numbering="1"
+            numbering="3"
             projectLink="https://snippix.netlify.app"
           />
           <ProjectCard
@@ -180,23 +192,17 @@ export const Projects = React.forwardRef((props, ref) => {
             content="Assistify is an Artificial Intelligence which uses COHERE
                 services to allows users to process documents and use top notch AI
                 models to talk with documents."
-            numbering="2"
+            numbering="4"
             projectLink="https://assistifyai.org"
           />
-          <ProjectCard
-            image="https://i.imgur.com/F8uNCoz.png"
-            title="Ayaan Designs"
-            content="A collaborative project with Product Designer, Ayaan. I developed a website to showcase his portfolio, using modern web technologies to ensure visual appeal and robust functionality."
-            numbering="3"
-            projectLink="https://ayaandesigns.xyz/"
-          />
+
           {isAllProjects && (
             <>
               <ProjectCard
                 image="https://i.imgur.com/wfTWOVk.png"
                 title="Nooderbot Landing Page"
                 content="A freelance project to develop a landing page for Nooderbot, a comprehensive Discord bot. The new design enhanced user interaction and increased website traffic by 60%."
-                numbering="4"
+                numbering="5"
                 projectLink="https://nooderbot.com"
               />
             </>
