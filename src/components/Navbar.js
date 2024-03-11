55a17a37b12b7d3f8444eb6867be41764bff0056
Preet Suthar <preetsutharxd@gmail.com>
2024-03-11 09:50:38 +0000

    Add open to work button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,9 +8,15 @@ export const Navbar = () => {
     <>
       <nav className="navbar">
         <Link href="/" className="navbar_icon section-1">
-          <Image src={website_logo} width={50} height={50} alt="Preet Suthar" />
+          <Image src={website_logo} width={45} height={45} alt="Preet Suthar" />
         </Link>
         <section className="section-2">
+          <div className="open_for_work">
+            <SmoothScrollLink href="#contact" className="transforming-link">
+              <span>Open for work</span>
+              <span>Open for work</span>
+            </SmoothScrollLink>
+          </div>
           <div className="navbar_links">
             <ul>
               <li className="navbar_link">
