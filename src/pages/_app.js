0630e7fee309d6307077596e881f4c43ade42c51
Preet Suthar <preetsutharxd@gmail.com>
2024-03-16 11:36:52 +0530

    Update package.json and add Partytown library

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -16,10 +16,31 @@ import "@/styles/components/_templates.scss";
 import { Layout } from "@/components/Layout";
 import ReactLenis from "@studio-freight/react-lenis";
 import { Providers } from "@/components/Providers";
+import Head from "next/head";
+import { Partytown } from "@builder.io/partytown/react";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <Partytown debug={true} forward={["dataLayer.push"]} />
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-Z16QM0TSSW"
+          type="text/partytown"
+        ></script>
+        <script
+          type="text/partytown"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-Z16QM0TSSW');
+            `,
+          }}
+        />
+      </Head>
       <svg className="grainy-filter pointer-events-none absolute cursor-none ">
         <filter id="grainy">
           <feTurbulence type="turbulence" baseFrequency="0.5"></feTurbulence>
