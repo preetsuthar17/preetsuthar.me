dc84985a16e8e3a931856be623b8110e33e4959a
Preet Suthar <preetsutharxd@gmail.com>
2024-03-25 13:54:20 +0530

    Refactor project card and button styles

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -120,8 +120,8 @@ const ProjectCard = ({ image, title, content, numbering, projectLink }) => {
           >
             <path
               fill="currentColor"
-              d="M5 21q-.825 0-1.412-.587T3 19V5q0-.825.588-1.412T5 3h7v2H5v14h14v-7h2v7q0 .825-.587 1.413T19 21zm4.7-5.3l-1.4-1.4L17.6 5H14V3h7v7h-2V6.4z"
-            ></path>
+              d="M6.4 18L5 16.6L14.6 7H6V5h12v12h-2V8.4z"
+            />
           </svg>{" "}
           View project
         </Link>
