796c879563a91083d047aaf05621a89ddfe366e9
Preet Suthar <preetsutharxd@gmail.com>
2024-05-04 14:33:22 +0530

    chore: Adjust scrollTrigger animation for CTA in About

--- src/components/About.js
+++ src/components/About.js
@@ -11,43 +11,45 @@ export const About = () => {
   const CTADivRef = useRef(null);
   useEffect(() => {
     gsap.to(CTADivRef.current, {
-      gap: "1rem",
+      bottom: "-3rem",
       ease: "circ.inOut",
       scrollTrigger: {
         trigger: ".hero",
         start: "center",
-        end: "bottom",
-        scrub: 0.9,
+        end: "bottom 10%",
+        scrub: true,
       },
     });
   }, []);
   return (
     <>
-      <section className="my-[10rem] py-[2rem] max-w-[65rem]  w-full mx-auto">
+      <section className="  max-w-[65rem]  w-full mx-auto relative">
         <div className="flex flex-wrap justify-between max-[770px]:flex-col">
-          <p className="text-4xl max-w-[60%]  leading-tight tracking-tighter p-4 max-[770px]:max-w-full">
+          <p className="text-4xl max-w-[60%]  leading-tight tracking-tighter p-4 max-[770px]:max-w-full relative">
             Helping brands to stand out in digital era. As a dedicated
             developer, I create standout digital experiences. Let's elevate your
             personal brand or project beyond the digital noise.
           </p>
-          <div
-            className="max-w-[30%]  max-[770px]:max-w-full flex flex-col justify-between items-center gap-[15rem]  max-[770px]:flex-row max-[770px]:ml-auto "
-            ref={CTADivRef}
-          >
-            <p className="p-4 text-2xl leading-tight tracking-tighter  max-[770px]:max-w-[60%] ">
+          <div className="max-w-[30%]  max-[770px]:max-w-full flex flex-col justify-between items-center gap-[15rem]  max-[770px]:flex-row max-[770px]:ml-auto ">
+            <p className="p-4 text-2xl leading-tight tracking-tighter  max-[770px]:max-w-[60%] relative">
               Don't waste your precious money on massive agencies, Let's build
               something today!
             </p>
-            <MagneticLink>
-              <Link
-                className="m-4 w-[8rem] h-[8rem] bg-[#ff7b00] text-white flex items-center justify-center rounded-full font-bold -rotate-6 "
-                href="#contact"
-              >
-                <span className="text-xl leading-5 text-center bricolage-fonts">
-                  Let's Talk
-                </span>
-              </Link>
-            </MagneticLink>
+            <div
+              className="absolute bottom-[-9rem] max-[770px]:right-[1rem]"
+              ref={CTADivRef}
+            >
+              <MagneticLink>
+                <Link
+                  className="m-4 w-[8rem] h-[8rem] bg-[#ff7b00] text-white flex items-center justify-center rounded-full font-bold -rotate-6 "
+                  href="#contact"
+                >
+                  <span className="text-xl leading-5 text-center bricolage-fonts">
+                    Let's Talk
+                  </span>
+                </Link>
+              </MagneticLink>
+            </div>
           </div>
         </div>
       </section>
