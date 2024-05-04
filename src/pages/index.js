ae9e593e8b0d114d29270ad1b60a015c5e3adb05
Preet Suthar <preetsutharxd@gmail.com>
2024-05-04 20:34:31 +0530

    feat: Add Services component to display available services

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import { About } from "@/components/About";
 import { Hero } from "@/components/Hero";
 import { IntroCurtains } from "@/components/IntroCurtains";
 import { Projects } from "@/components/Projects";
+import { Services } from "@/components/Services";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Hero />
       <About />
       <Projects />
+      <Services />
     </>
   );
 }
