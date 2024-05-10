eb4fa522b738db61c6ad3fe1918618de3c97fa0e
Preet Suthar <preetsutharxd@gmail.com>
2024-05-10 08:05:37 +0530

    refactor: Update ProjectsCard component to use a larger font size for the description

--- src/components/Projcts.js
+++ src/components/Projcts.js
@@ -8,7 +8,7 @@ export const ProjectsCard = ({ project }) => {
         className="p-4 transition-all hover:bg-zinc-950 cursor-pointer rounded-xl duration-[0.2s]"
       >
         <h3 className="text-lg font-semibold">{project.title}</h3>
-        <p className="text-sm opacity-80">{project.description}</p>
+        <p className="text-[14px] opacity-80">{project.description}</p>
       </div>
     </>
   );
