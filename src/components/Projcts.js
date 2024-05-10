ab1dfab1448e3dc43dff689d8d768d998926036b
Preet Suthar <preetsutharxd@gmail.com>
2024-05-10 08:39:48 +0530

    refactor: Update ProjectsCard component to use a slightly faded font for the title

--- src/components/Projcts.js
+++ src/components/Projcts.js
@@ -7,8 +7,39 @@ export const ProjectsCard = ({ project }) => {
         }}
         className="p-4 transition-all hover:bg-zinc-950 cursor-pointer rounded-xl duration-[0.2s]"
       >
-        <h3 className="text-lg font-semibold opacity-95">{project.title}</h3>
-        <p className="text-[14px] opacity-80">{project.description}</p>
+        <h3 className="font-semibold text-md opacity-95">{project.title}</h3>
+        <p className="text-[14px] opacity-80 leading-tight">
+          {project.description}
+        </p>
+      </div>
+    </>
+  );
+};
+
+export const FreelanceProjectsCard = ({ project }) => {
+  return (
+    <>
+      <div
+        onClick={() => {
+          window.open(project.url, "_blank");
+        }}
+        className="p-4 transition-all hover:bg-zinc-950 cursor-pointer rounded-xl duration-[0.2s]"
+      >
+        <h3 className="font-semibold text-md opacity-95">{project.title}</h3>
+        <p className="flex items-center gap-[3px] text-sm opacity-80">
+          view project{" "}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="18"
+            height="18"
+            viewBox="0 0 24 24"
+          >
+            <path
+              fill="currentColor"
+              d="M6.4 18L5 16.6L14.6 7H6V5h12v12h-2V8.4z"
+            />
+          </svg>
+        </p>
       </div>
     </>
   );
@@ -25,17 +56,62 @@ export const Projects = () => {
     {
       title: "HextaUI",
       description:
-        "Elevate your projects with stunning, modern components. Simply copy and paste these responsive, beautiful elements for instant awesomeness",
+        "Copy and paste some Stunning & Modern components in your website.",
       url: "https://ui.hextastudio.in",
     },
   ];
+
+  const FreelanceProjects = [
+    {
+      title: "Nooderbot website",
+      url: "https://nooderbot.com",
+    },
+    {
+      title: "Vultrex website",
+      url: "https://beta.vultrex.dev",
+    },
+    {
+      title: "YADMB bot website",
+      url: "https://yadmb.xyz",
+    },
+    {
+      title: "Impenny website",
+      url: "https://impenny.me",
+    },
+    {
+      title: "Bidme bot website",
+      url: "https://bidme.bot",
+    },
+    {
+      title: "Hood Ecommerce website",
+      url: "https://hood.pw",
+    },
+  ];
+
   return (
     <>
-      <section className="flex flex-col py-10 gap-7">
-        <h2 className="px-4 text-2xl font-bold tracking-tight ">Projects</h2>
+      <section className="flex flex-col gap-10 py-10">
         <div className="flex flex-col gap-2">
+          <h2 className="px-4 text-2xl font-bold tracking-tight py-7 h2">
+            Projects
+          </h2>
           {projects.map((project, index) => (
             <ProjectsCard
+              key={index}
+              project={{
+                title: project.title,
+                description: project.description,
+                url: project.url,
+              }}
+            />
+          ))}
+        </div>
+        <div className="flex flex-col gap-2">
+          <h2 className="px-4 text-2xl font-bold tracking-tight py-7 h2">
+            Freelance Work
+          </h2>
+          {FreelanceProjects.map((project, index) => (
+            <FreelanceProjectsCard
               key={index}
               project={{
                 title: project.title,
