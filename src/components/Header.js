f54ac4bd25f1829ff13566983f9548c7f2b6045c
Preet Suthar <preetsutharxd@gmail.com>
2024-05-10 10:03:23 +0530

    refactor: Add TooltipComponent to Header component for social media links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { FaTwitter, FaGithub, FaDiscord } from "react-icons/fa";
+import { TooltipComponent } from "./hexta-ui/Tooltip";
 
 export const Header = () => {
   return (
@@ -13,23 +14,29 @@ export const Header = () => {
           <Link
             href="https://twitter.com/preetsuthar17"
             target="_blank"
-            className="p-2 transition-all rounded hover:bg-zinc-900 duration-[0.2s]"
+            className="p-2 transition-all rounded hover:bg-zinc-900 duration-[0.2s] flex items-center h-fit m-auto"
           >
-            <FaTwitter size={20} className="text-gray-300" />
+            <TooltipComponent direction="bottom" text="Twitter (x)">
+              <FaTwitter size={20} className="text-gray-300" />
+            </TooltipComponent>
           </Link>
           <Link
             href="https://github.com/preetsuthar17"
             target="_blank"
             className="p-2 transition-all rounded hover:bg-zinc-900 duration-[0.2s]"
           >
-            <FaGithub size={20} className="text-gray-300" />
+            <TooltipComponent direction="bottom" text="Github">
+              <FaGithub size={20} className="text-gray-300" />
+            </TooltipComponent>
           </Link>
           <Link
             href="https://dsc.gg/preet"
             target="_blank"
             className="p-2 transition-all rounded hover:bg-zinc-900 duration-[0.2s]"
           >
-            <FaDiscord size={20} className="text-gray-300" />
+            <TooltipComponent direction="bottom" text="Discord">
+              <FaDiscord size={20} className="text-gray-300" />
+            </TooltipComponent>
           </Link>
         </div>
       </header>
