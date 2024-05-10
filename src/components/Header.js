fede4fb0951f5daf364b75e6e0231521ec8b87d4
Preet Suthar <preetsutharxd@gmail.com>
2024-05-10 08:39:52 +0530

    refactor: Update Header component with improved styling and social media links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,32 @@
 import Link from "next/link";
-import { FaGit, FaTwitter } from "react-icons/fa";
-import { FaGithub } from "react-icons/fa";
-import { FaDiscord } from "react-icons/fa";
+import { FaTwitter, FaGithub, FaDiscord } from "react-icons/fa";
 
 export const Header = () => {
   return (
     <>
-      <header className="flex flex-wrap items-center justify-between px-4 py-10">
-        <div>
+      <header className="flex flex-wrap items-center justify-between p-4 bg-zinc-950 mt-[2rem] rounded-xl border border-zinc-900 gap-2">
+        <div className="flex flex-col justify-center ">
           <h1 className="text-3xl font-bold tracking-tight">Preet Suthar</h1>
           <p className="text-sm text-zinc-300">Creative Front-end developer</p>
         </div>
         <div className="flex gap-2">
           <Link
-            href="#"
-            className="p-2 transition-all rounded hover:bg-zinc-950 duration-[0.2s]"
+            href="https://twitter.com/preetsuthar17"
+            target="_blank"
+            className="p-2 transition-all rounded hover:bg-zinc-900 duration-[0.2s]"
           >
             <FaTwitter size={20} className="text-gray-300" />
           </Link>
           <Link
-            href="#"
+            href="https://github.com/preetsuthar17"
+            target="_blank"
             className="p-2 transition-all rounded hover:bg-zinc-900 duration-[0.2s]"
           >
             <FaGithub size={20} className="text-gray-300" />
           </Link>
           <Link
-            href="#"
+            href="https://dsc.gg/preet"
+            target="_blank"
             className="p-2 transition-all rounded hover:bg-zinc-900 duration-[0.2s]"
           >
             <FaDiscord size={20} className="text-gray-300" />
