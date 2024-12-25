5645c90ddc5aaca5bc282b97fc4f9b8ce5ebfa13
preetsuthar17 <preetsutharxd@gmail.com>
2024-12-25 17:30:11 +0530

    feat: remove ThemeToggle component and associated dark mode logic; clean up global styles

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,31 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-                (function() {
-                  function getInitialTheme() {
-                    if (typeof localStorage !== 'undefined' && localStorage.getItem('theme')) {
-                      return localStorage.getItem('theme');
-                    }
-                    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-                      return 'dark';
-                    }
-                    return 'light';
-                  }
-                  
-                  const theme = getInitialTheme();
-                  
-                  if (theme === 'dark') {
-                    document.documentElement.classList.add('dark');
-                  } else {
-                    document.documentElement.classList.remove('dark');
-                  }
-                })();
-              `,
-          }}
-        />
         <meta httpEquiv="Accept-CH" content="DPR, Width, Viewport-Width" />
         <link
           rel="preconnect"
