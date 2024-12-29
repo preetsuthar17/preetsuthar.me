ffae8e1dd96f7e1b1625e9527ea76d7da414e16f
preetsuthar17 <preetsutharxd@gmail.com>
2024-12-29 20:14:56 +0530

    fix: enhance layout and add links for viewing all projects and posts in Work component

--- src/components/Work.js
+++ src/components/Work.js
@@ -48,8 +48,8 @@ const working = [
 export default function work({ posts }) {
   return (
     <>
-      <article className="flex justify-between flex-wrap gap-6  text-left">
-        <div className="flex-1">
+      <article className="flex justify-between flex-wrap gap-6 text-left">
+        <div className="flex-1 flex flex-col items-start justify-start gap-8">
           <h2 className="font-medium opacity-80 mb-4">Working</h2>
           <ul className="flex flex-col gap-4">
             {working.map((project, index) => (
@@ -67,10 +67,10 @@ export default function work({ posts }) {
             ))}
           </ul>
         </div>
-        <div className="flex-1">
+        <div className="flex-1 flex flex-col items-start justify-start gap-8">
           <h2 className="font-medium opacity-80 mb-4">Projects</h2>
           <ul className="flex flex-col gap-4">
-            {projects.map((project, index) => (
+            {projects.slice(0, 5).map((project, index) => (
               <li key={index} className="flex flex-col">
                 <Link
                   className="flex items-center gap-1 font-medium font-inter"
@@ -84,22 +84,31 @@ export default function work({ posts }) {
               </li>
             ))}
           </ul>
+          <Link href="/writing" className="font-medium font-sans underline">
+            View All Projects
+          </Link>
         </div>
-        <div className="flex-1">
-          <h2 className="font-medium opacity-80 mb-4">Writing</h2>
+        <div className="flex-1 flex flex-col items-start justify-start gap-8">
+          <h2 className="font-medium opacity-80">Writing</h2>
           <ul className="flex flex-col gap-4">
-            {posts?.map((post) => (
+            {posts?.slice(0, 5).map((post) => (
               <li key={post.slug}>
                 <Link
                   className="flex items-center gap-1 font-medium font-inter"
                   href={`/writing/${post.slug}`}
                 >
-                  {post.title} <span><FiArrowUpRight size={18} /></span>
+                  {post.title}{" "}
+                  <span>
+                    <FiArrowUpRight size={18} />
+                  </span>
                 </Link>
                 <p className="text-sm opacity-80 mt-1">{post.excerpt}</p>
               </li>
             ))}
           </ul>
+          <Link href="/writings" className="font-medium font-sans underline">
+            View All Posts
+          </Link>
         </div>
       </article>
     </>
