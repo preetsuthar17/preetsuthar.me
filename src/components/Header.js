0a85046440f0119f78b8cd15b497295cf859e5fe
preetsuthar17 <preetsutharxd@gmail.com>
2025-01-02 19:47:38 +0530

    feat: add tooltip component and social links to Header for improved user interaction

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,41 @@
 import Link from "next/link";
+
 import { SpotifyStatus } from "./SpotifyStatus";
 
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+import { Instagram, Twitter, Youtube, Github } from "lucide-react";
+import { FaInstagram, FaTwitter, FaYoutube, FaGithub } from "react-icons/fa";
+
 const Header = () => {
+  const links = [
+    {
+      name: "Twitter",
+      icon: <FaTwitter size={20} className="opacity-80 hover:opacity-100 hover:fill-[#1c96e9] transition-all"/>,
+      link: "https://twitter.com/preetsuthar17",
+    },
+    {
+      name: "YouTube",
+      icon: <FaYoutube size={20} className="opacity-80 hover:opacity-100 hover:fill-[#ff0033] transition-all"/>,
+      link: "https://youtube.com/preetsuthar17",
+    },
+    {
+      name: "GitHub",
+      icon: <FaGithub size={20} className="opacity-80 hover:opacity-100 hover:fill-[#252a2f] transition-all" />,
+      link: "https://github.com/preetsuthar17",
+    },
+    {
+      name: "Instagram",
+      icon: <FaInstagram size={20} className="opacity-80 hover:opacity-100 hover:bg-gradient-to-r from-[#833ab4] via-[#fd1d1d] to-[#fcb045] hover:fill-white rounded-md transition-all "/>,
+      link: "https://instagram.com/preetsuthar17",
+    },
+  ];
+
   return (
     <>
       <header className="flex flex-col gap-6 text-justify text-pretty">
@@ -19,27 +53,19 @@ const Header = () => {
           <em className="[font:var(--type-italic)]">(SaaS)</em>  in the last 5
           years, each focusing on easy-to-use and secure tech solutions.
         </p>
-        <div>
-          <Link
-            href="https://twitter.com/preetsuthar"
-            className="opacity-80 hover:opacity-100 transition-all underline"
-          >
-            Twitter
-          </Link>
-          <span className="mx-2">•</span>
-          <Link
-            href="https://www.linkedin.com/in/preetsuthar/"
-            className="opacity-80 hover:opacity-100 transition-all underline"
-          >
-            YouTube
-          </Link>
-          <span className="mx-2">•</span>
-          <Link
-            href="https://hextastudio.in"
-            className="opacity-80 hover:opacity-100 transition-all underline"
-          >
-            HextaStudio
-          </Link>
+        <div className="flex items-center gap-4">
+          {links.map((link, index) => (
+            <TooltipProvider key={index} delayDuration={100}>
+              <Tooltip>
+                <TooltipTrigger>
+                  <Link href={`${link.link}`} target="_blank">
+                    {link.icon}
+                  </Link>
+                </TooltipTrigger>
+                <TooltipContent sideOffset={8}>{link.name}</TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+          ))}
         </div>
       </header>
     </>
