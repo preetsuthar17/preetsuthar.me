1b93c592aa504d88a42af26d500286accff8ef2c
preetsuthar17 <preetsutharxd@gmail.com>
2025-01-24 10:50:40 +0530

    feat: add Hero Design entry for bolt.new AI website to designs data

--- src/data/web-design-dev.js
+++ src/data/web-design-dev.js
@@ -11,6 +11,12 @@ const webDesignDev = [
     link: "https://kikxai.netlify.app/",
     image: "https://i.imgur.com/y9M4sy0.png",
   },
+  {
+    title: "Hero Design for bolt.new AI website",
+    description: "Roast design for bolt.new AI website.",
+    image: "https://i.imgur.com/xUBflPl.png",
+    link: "https://dribbble.com/shots/25456845-Hero-Design-for-bolt-new-AI-website",
+  },
 ];
 
 export default webDesignDev;
