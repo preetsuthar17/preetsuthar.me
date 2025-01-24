56906694c53070ba0987fc643e74d707335a7b2c
preetsuthar17 <preetsutharxd@gmail.com>
2025-01-24 10:52:31 +0530

    feat: add new Hero Design entry for cronq.qi website to designs data

--- src/data/web-design-dev.js
+++ src/data/web-design-dev.js
@@ -1,5 +1,17 @@
 const webDesignDev = [
   {
+    title: "Hero Design for bolt.new AI website",
+    description: "Roast design for bolt.new AI website.",
+    image: "https://i.imgur.com/xUBflPl.png",
+    link: "https://dribbble.com/shots/25456845-Hero-Design-for-bolt-new-AI-website",
+  },
+  {
+    title: "Hero design for cronq.qi website",
+    description: "Roast design for cronq.qi website.",
+    image: "https://i.imgur.com/JhSy5Q9.png",
+    link: "https://x.com/preetsuthar17/status/1882655165016404178",
+  },
+  {
     title: "HextaUI",
     description: "Build stunning websites with less effort.",
     link: "https://hextaui.com",
@@ -10,12 +22,6 @@ const webDesignDev = [
     description: "Step Forward Your Hiring Process",
     link: "https://kikxai.netlify.app/",
     image: "https://i.imgur.com/y9M4sy0.png",
-  },
-  {
-    title: "Hero Design for bolt.new AI website",
-    description: "Roast design for bolt.new AI website.",
-    image: "https://i.imgur.com/xUBflPl.png",
-    link: "https://dribbble.com/shots/25456845-Hero-Design-for-bolt-new-AI-website",
   },
 ];
 
