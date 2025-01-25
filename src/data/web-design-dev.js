a61a6b4ab59f04d9349fae80559362a4bd6f181d
preetsuthar17 <preetsutharxd@gmail.com>
2025-01-25 12:46:20 +0530

    feat: add new Hero Design entries for FeedWall and kikxAI to designs data

--- src/data/web-design-dev.js
+++ src/data/web-design-dev.js
@@ -6,6 +6,18 @@ const webDesignDev = [
     link: "https://dribbble.com/shots/25456845-Hero-Design-for-bolt-new-AI-website",
   },
   {
+    title: "Hero Design for FeedWall - PlayFul & Clean",
+    description: "Roast design for FeedWall website.",
+    image: "https://i.imgur.com/uOyd2nI.png",
+    link: "https://dribbble.com/shots/25529263-Hero-Design-for-FeedWall-PlayFul-Clean",
+  },
+  {
+    title: "kikxAI",
+    description: "Step Forward Your Hiring Process",
+    link: "https://kikxai.netlify.app/",
+    image: "https://i.imgur.com/y9M4sy0.png",
+  },
+  {
     title: "Hero design for cronq.qi website",
     description: "Roast design for cronq.qi website.",
     image: "https://i.imgur.com/JhSy5Q9.png",
@@ -16,12 +28,6 @@ const webDesignDev = [
     description: "Build stunning websites with less effort.",
     link: "https://hextaui.com",
     image: "https://i.imgur.com/803BRuM.png",
-  },
-  {
-    title: "kikxAI",
-    description: "Step Forward Your Hiring Process",
-    link: "https://kikxai.netlify.app/",
-    image: "https://i.imgur.com/y9M4sy0.png",
   },
 ];
 
