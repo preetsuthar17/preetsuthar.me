8da2e1cf75d7e5f467bce685437028e3613054d8
preetsuthar17 <preetsutharxd@gmail.com>
2025-01-28 10:41:11 +0530

    fix: update titles to use 'Hero Redesign' for consistency in web design data

--- src/data/web-design-dev.js
+++ src/data/web-design-dev.js
@@ -12,19 +12,19 @@ const webDesignDev = [
     link: "https://dribbble.com/shots/25533470-Dodo-Payments-Hero-Redesign",
   },
   {
-    title: "Hero Design for FeedWall - PlayFul & Clean",
+    title: "Hero Redesign for FeedWall - PlayFul & Clean",
     description: "Roast design for FeedWall website.",
     image: "https://i.imgur.com/uOyd2nI.png",
     link: "https://dribbble.com/shots/25529263-Hero-Design-for-FeedWall-PlayFul-Clean",
   },
   {
-    title: "Hero Design For Hero Design for Sonā - Sleek & Modern",
+    title: "Hero Redesign For Hero Design for Sonā - Sleek & Modern",
     description: "Roast design for Sonā website.",
     image: "https://i.imgur.com/NL7mM4s.png",
     link: "https://dribbble.com/shots/25530624-Hero-Design-for-Son-Sleek-Modern",
   },
   {
-    title: "Hero Design for bolt.new AI website",
+    title: "Hero Redesign for bolt.new AI website",
     description: "Roast design for bolt.new AI website.",
     image: "https://i.imgur.com/xUBflPl.png",
     link: "https://dribbble.com/shots/25456845-Hero-Design-for-bolt-new-AI-website",
@@ -34,12 +34,6 @@ const webDesignDev = [
     description: "Step Forward Your Hiring Process",
     link: "https://kikxai.netlify.app/",
     image: "https://i.imgur.com/y9M4sy0.png",
-  },
-  {
-    title: "Hero design for cronq.qi website",
-    description: "Roast design for cronq.qi website.",
-    image: "https://i.imgur.com/JhSy5Q9.png",
-    link: "https://x.com/preetsuthar17/status/1882655165016404178",
   },
   {
     title: "HextaUI",
