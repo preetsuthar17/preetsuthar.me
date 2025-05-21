df87fb94dca9892e6a18d08587713913e0e8d3e8
preetsuthar17 <preetsutharxd@gmail.com>
2025-05-21 09:23:25 +0530

    refactor(saas): remove outdated SaaS entries for improved relevance

--- src/data/saas.js
+++ src/data/saas.js
@@ -14,31 +14,6 @@ const saas = [
     description: "Web design and development agency.",
     link: "https://hextastudio.com",
   },
-  {
-    title: "IndieHub",
-    description: "The only Hub made for Indie Hackers.",
-    link: "https://indiehub.hextastudio.in/",
-  },
-  {
-    title: "RateThisTool",
-    description: "Effortless feedback system for your SaaS.",
-    link: "https://ratethistool.vercel.app",
-  },
-  {
-    title: "ContraFast",
-    description: "Create & Sign Contracts 10x Faster.",
-    link: "https://contrafast.in",
-  },
-  {
-    title: "FlixFolio",
-    description: "Create a Portfolio Without Writing Code.",
-    link: "https://flixfolio.in",
-  },
-  {
-    title: "NextLaunch",
-    description: "Launch Your SaaS Faster Than Ever.",
-    link: "https://nextlaunch.hextastudio.in",
-  },
 ];
 
 export default saas;
