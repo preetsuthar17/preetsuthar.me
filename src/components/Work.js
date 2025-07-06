eca3e667deafc8013d582ceab930377885c45510
preetsuthar17 <preetsutharxd@gmail.com>
2025-07-06 20:34:50 +0530

    feat: add hover muting effect to SaaS and blog post lists for improved user interaction

--- src/components/Work.js
+++ src/components/Work.js
@@ -28,11 +28,29 @@ export default function work({ posts }) {
           {/* divider */}
 
           <div className="flex flex-col items-center w-full justify-between text-left gap-6 px-10">
-            <ul className="flex flex-col gap-5 w-full">
-              {saas.map((saas, index) => (
+            {/* SaaS list with hover muting effect */}
+            <ul className="flex flex-col gap-5 w-full group/saas-list">
+              {saas.map((saas, idx) => (
                 <li
-                  key={index}
-                  className="flex items-center text-left gap-2 w-full justify-between  max-[590px]:flex-col max-[590px]:items-start"
+                  key={idx}
+                  className="flex items-center text-left gap-2 w-full justify-between max-[590px]:flex-col max-[590px]:items-start group/saas-item transition-opacity duration-300"
+                  onMouseEnter={(e) => {
+                    const parent = e.currentTarget.parentElement;
+                    if (parent) {
+                      Array.from(parent.children).forEach((li, i) => {
+                        if (i !== idx) li.classList.add("opacity-40");
+                        else li.classList.remove("opacity-40");
+                      });
+                    }
+                  }}
+                  onMouseLeave={(e) => {
+                    const parent = e.currentTarget.parentElement;
+                    if (parent) {
+                      Array.from(parent.children).forEach((li) => {
+                        li.classList.remove("opacity-40");
+                      });
+                    }
+                  }}
                 >
                   <Link
                     className="flex items-center gap-1 font-medium justify-between text-[var(--blue-color)]  max-[590px]:w-full"
@@ -63,11 +81,29 @@ export default function work({ posts }) {
           {/* divider */}
 
           <div className="flex flex-col items-center w-full justify-between text-left gap-6 px-10">
-            <ul className="flex flex-col gap-5 w-full">
-              {posts?.map((post) => (
+            {/* Blog posts list with hover muting effect */}
+            <ul className="flex flex-col gap-5 w-full group/blog-list">
+              {posts?.map((post, idx) => (
                 <li
                   key={post.slug}
-                  className="flex items-center text-left gap-2 w-full justify-between max-[590px]:flex-col max-[590px]:items-start"
+                  className="flex items-center text-left gap-2 w-full justify-between max-[590px]:flex-col max-[590px]:items-start group/blog-item transition-opacity duration-300"
+                  onMouseEnter={(e) => {
+                    const parent = e.currentTarget.parentElement;
+                    if (parent) {
+                      Array.from(parent.children).forEach((li, i) => {
+                        if (i !== idx) li.classList.add("opacity-40");
+                        else li.classList.remove("opacity-40");
+                      });
+                    }
+                  }}
+                  onMouseLeave={(e) => {
+                    const parent = e.currentTarget.parentElement;
+                    if (parent) {
+                      Array.from(parent.children).forEach((li) => {
+                        li.classList.remove("opacity-40");
+                      });
+                    }
+                  }}
                 >
                   <Link
                     className="flex items-center gap-1 font-medium justify-between text-[var(--blue-color)] max-[590px]:w-full"
